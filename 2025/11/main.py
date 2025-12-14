6e87f9cdd2c579dafc62e52c78bd64becbdc29a7
Matt Goldman <matt@carsandbids.com>
2025-12-14 14:47:30 -0500

    day 11

---
+++ 2025/11/main.py
@@ -0,0 +1,60 @@
+from functools import cache
+
+
+def parse(name="input.txt"):
+    graph = {"out": []}
+
+    with open(name) as f:
+        for line in f:
+            node, rest = line.strip().split(": ")
+            children = rest.split(" ")
+            graph[node] = children
+
+    return graph
+
+GRAPH = {}
+
+@cache
+def find_paths(start, end):
+
+    if start == end:
+        return 1
+    else:
+        return sum([find_paths(n, end) for n in GRAPH[start]])
+ 
+
+def part1(graph):
+    global GRAPH
+    GRAPH = graph
+    find_paths.cache_clear()
+    return find_paths("you", "out")
+
+
+def part2(graph):
+    global GRAPH
+    GRAPH = graph
+    find_paths.cache_clear()
+    dac_fft = find_paths("dac", "fft")
+
+    if dac_fft:
+        return (
+            find_paths("svr", "dac") *
+            dac_fft *
+            find_paths("fft", "out")
+        )
+    else:
+        return (
+            find_paths("svr", "fft") *
+            find_paths("fft", "dac") *
+            find_paths("dac", "out")
+        )
+
+
+graph = parse("ex_input.txt")
+assert 5 == part1(graph)
+graph = parse("ex_input2.txt")
+assert 2 == part2(graph)
+
+graph = parse()
+print(part1(graph))
+print(part2(graph))
